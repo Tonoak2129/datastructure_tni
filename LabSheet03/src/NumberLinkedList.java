ef6b9aadb46db80331dcee4aa28dc2b86513f67e
Ton-Oak <tonoak2129@gmail.com>
2026-07-20 21:00:33 +0700

    Add LabSheet03

---
+++ LabSheet03/src/NumberLinkedList.java
@@ -0,0 +1,41 @@
+
+public class NumberLinkedList {
+
+    public static void main(String[] args) {
+        LinkedList numbers = new LinkedList();
+
+        numbers.insert(0,37);
+        numbers.insert(0,7);
+        numbers.insert(0,4);
+        numbers.insert(0,16);
+
+
+        System.out.println(numbers.traversal());
+
+        numbers.insert(2,20);
+        System.out.println(numbers.traversal());
+
+        numbers.insert(25);
+        System.out.println(numbers.traversal());
+
+        numbers.remove(0);
+        System.out.println(numbers.traversal());
+
+        numbers.remove(2);
+        System.out.println(numbers.traversal());
+
+        numbers.removeLastElement();
+        System.out.println(numbers.traversal());
+
+        System.out.println("\nLength of elements in List= " + numbers.length());
+
+        System.out.println("Element in index 1        = " + numbers.get(1));
+
+        numbers.set(1,100);
+        System.out.println("Updated index 1 to 100    = " + numbers.traversal());
+
+        numbers.clear();
+        System.out.println(numbers.traversal());
+    }
+
+}
