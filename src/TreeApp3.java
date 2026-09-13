64f119fb964d8e6f31aa2673cb257faaf70ae231
Tonoak2129 <96166620+Tonoak2129@users.noreply.github.com>
2026-09-13 14:43:06 +0700

    LabSheet09

---
+++ src/TreeApp3.java
@@ -0,0 +1,19 @@
+public class TreeApp3 {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        System.out.println("----------------------------");
+        System.out.println("Binary tree from createTree5()");
+        System.out.println("----------------------------");
+        tree.createTree5();
+        tree.printTree(tree.getRoot(), 0);
+
+        tree.delete(25);
+        tree.printTree(tree.getRoot(), 0);
+
+        tree.delete(40);
+        tree.printTree(tree.getRoot(), 0);
+
+        tree.delete(60);
+        tree.printTree(tree.getRoot(), 0);
+    }
+}
